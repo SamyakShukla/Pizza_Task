82ec755db38b2c7bbcead135e34ff324b1fecb9d
Samyak Shukla <E20CSE023@bennett.edu.in>
2023-09-16 18:57:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React, { useState } from 'react';
 import './App.css';
+import PizzaBase from './components/PizzaBase';
 
 function App() {
+  const [selectedBase, setSelectedBase] = useState(null);
+
+  const baseImages = {
+    'Thin Crust': '/images/thincrust.svg',
+    'Pan Crust': '/images/pancrust.svg',
+  };
+
+  const handleBaseDragStart = (base) => {
+    setSelectedBase(base);
+  };
+
+  const handleCircleDrop = (e) => {
+    e.preventDefault();
+    // setSelectedBase(null);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>We build Pizzas</h1>
+      <div className="pizza-builder">
+        <div className="base-options">
+          <h2>Available Bases</h2>
+          <PizzaBase base="Thin Crust" onDragStart={handleBaseDragStart} />
+          <PizzaBase base="Pan Crust" onDragStart={handleBaseDragStart} />
+        </div>
+        <div
+          className="empty-circle"
+          onDrop={handleCircleDrop}
+          onDragOver={(e) => e.preventDefault()}
         >
-          Learn React
-        </a>
-      </header>
+          {selectedBase ? (
+            <div>
+              <img
+                src={process.env.PUBLIC_URL + baseImages[selectedBase]}
+                alt={`${selectedBase} Base`}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                
+              />
+              <p>{selectedBase} Base</p>
+            </div>
+          ) : (
+            <p>Drop Base Here</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/PizzaBase.js
@@ -0,0 +1,18 @@
+// src/components/PizzaBase.js
+
+import React from 'react';
+
+function PizzaBase({ base, onDragStart }) {
+  return (
+    <div
+      className="pizza-base"
+      draggable
+      onDragStart={() => onDragStart(base)}
+    >
+       
+      {base}
+    </div>
+  );
+}
+
+export default PizzaBase;
